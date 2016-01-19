aed7ae0bd47a93e36837489a4293950947855d2e
techbrew <mark.a.woodman@gmail.com>
2016-01-19 15:51:05 -0700

    Added logging

--- src/main/java/example/mod/ExampleMod.java
+++ src/main/java/example/mod/ExampleMod.java
@@ -20,21 +20,23 @@
 
 package example.mod;
 
-/**
- * Example example.mod showing how to use the JourneyMap API.
- */
-
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
+/**
+ * Example mod showing how to use the JourneyMap API.
+ */
 @Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
 public class ExampleMod
 {
-    public static final String MODID = "examplemod";
+    public static final String MODID = "examplemod-jm";
     public static final String VERSION = "@API_VERSION@";
+    public static final Logger LOGGER  = LogManager.getFormatterLogger(MODID);
 
     @Mod.Instance(ExampleMod.MODID)
     public static ExampleMod instance;
@@ -46,17 +48,20 @@ public class ExampleMod
     public void preInit(FMLPreInitializationEvent event)
     {
         proxy.preInit(event);
+        LOGGER.info("ExampleMod preInit done");
     }
 
     @Mod.EventHandler
     public void init(FMLInitializationEvent event)
     {
         proxy.init(event);
+        LOGGER.info("ExampleMod init done");
     }
 
     @Mod.EventHandler
     public void postInit(FMLPostInitializationEvent event)
     {
         proxy.postInit(event);
+        LOGGER.info("ExampleMod postInit done");
     }
 }
