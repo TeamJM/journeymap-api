1af697985cb0f118b082f70d2927eb910b2be1db
techbrew <mark.a.woodman@gmail.com>
2016-02-11 16:30:36 -0700

    Added displayCenter and displayBounds to UIState

--- src/main/java/journeymap/client/api/util/UIState.java
+++ src/main/java/journeymap/client/api/util/UIState.java
@@ -6,6 +6,8 @@ import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.util.BlockPos;
 
 import javax.annotation.Nullable;
+import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
 
 /**
  * Provides the current state of a Map UI in JourneyMap.
@@ -48,6 +50,17 @@ public final class UIState
     public final AxisAlignedBB blockBounds;
 
     /**
+     * The center pixel of the UI.  If active==false, this will be null.
+     */
+    public final Point2D.Double displayCenter;
+
+    /**
+     * The screen area (pixels) used by the UI.  If active==false, this will be null.
+     */
+    public final Rectangle2D.Double displayBounds;
+
+
+    /**
      * Constructor.
      *
      * @param ui          The UI which has been updated.
@@ -61,7 +74,9 @@ public final class UIState
     public UIState(Context.UI ui, boolean active, int dimension, int zoom,
                    @Nullable Context.MapType mapType,
                    @Nullable BlockPos mapCenter,
-                   @Nullable AxisAlignedBB blockBounds)
+                   @Nullable AxisAlignedBB blockBounds,
+                   @Nullable Point2D.Double displayCenter,
+                   @Nullable Rectangle2D.Double displayBounds)
     {
         this.ui = ui;
         this.active = active;
@@ -70,6 +85,19 @@ public final class UIState
         this.mapType = mapType;
         this.mapCenter = mapCenter;
         this.blockBounds = blockBounds;
+        this.displayCenter = displayCenter;
+        this.displayBounds = displayBounds;
+    }
+
+    /**
+     * Convenience factory method to create an inactive UIState.
+     *
+     * @param ui the ui
+     * @return a UIState
+     */
+    public static UIState inactive(Context.UI ui)
+    {
+        return new UIState(ui, false, 0, 0, null, null, null, null, null);
     }
 
     @Override
@@ -89,14 +117,14 @@ public final class UIState
                 Objects.equal(zoom, mapState.zoom) &&
                 Objects.equal(ui, mapState.ui) &&
                 Objects.equal(mapType, mapState.mapType) &&
-                Objects.equal(mapCenter, mapState.mapCenter) &&
-                Objects.equal(blockBounds, mapState.blockBounds);
+                Objects.equal(displayCenter, mapState.displayCenter) &&
+                Objects.equal(displayBounds, mapState.displayBounds);
     }
 
     @Override
     public int hashCode()
     {
-        return Objects.hashCode(ui, active, dimension, zoom, mapType, mapCenter, blockBounds);
+        return Objects.hashCode(ui, active, dimension, zoom, mapType, displayCenter, displayBounds);
     }
 
     @Override
@@ -110,6 +138,8 @@ public final class UIState
                 .add("dimension", dimension)
                 .add("mapCenter", mapCenter)
                 .add("blockBounds", blockBounds)
+                .add("displayCenter", displayCenter)
+                .add("displayBounds", displayBounds)
                 .toString();
     }
 
