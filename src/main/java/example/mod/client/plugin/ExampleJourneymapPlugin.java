dae050d6dcb99f1f9113fe2f7b25e65bcf6f6f02
techbrew <mark.a.woodman@gmail.com>
2016-02-12 22:38:50 -0700

    Minor behavior changes

--- src/main/java/example/mod/client/plugin/ExampleJourneymapPlugin.java
+++ src/main/java/example/mod/client/plugin/ExampleJourneymapPlugin.java
@@ -86,7 +86,7 @@ public class ExampleJourneymapPlugin implements journeymap.client.api.IClientPlu
     {
         try
         {
-            ExampleMod.LOGGER.info("ClientEvent: " + event);
+            ExampleMod.LOGGER.debug("ClientEvent: " + event);
 
             switch (event.type)
             {
