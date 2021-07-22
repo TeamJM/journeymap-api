b788e4641243a0daf624d55543848445583ab7f8
Mysticdrew <drewhaas@gmail.com>
2021-07-22 17:03:31 -0500

    updated hard coded versions

--- src/main/java/example/mod/ExampleMod.java
+++ src/main/java/example/mod/ExampleMod.java
@@ -44,7 +44,7 @@ import org.apache.logging.log4j.Logger;
 public class ExampleMod
 {
     public static final String MODID = "examplemod-jm";
-    public static final String VERSION = "1.6";
+    public static final String VERSION = "1.7";
     public static final Logger LOGGER = LogManager.getFormatterLogger(MODID);
 
 
