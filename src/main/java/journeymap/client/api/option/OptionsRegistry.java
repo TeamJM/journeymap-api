03006d9c6c6b635255079485dfd03a6b51cea9d9
Mysticdrew <drewhaas@gmail.com>
2022-01-22 09:16:07 -0600

    deprecated OptionsRegistry so modders know not to use it

--- src/main/java/journeymap/client/api/option/OptionsRegistry.java
+++ src/main/java/journeymap/client/api/option/OptionsRegistry.java
@@ -3,10 +3,20 @@ package journeymap.client.api.option;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * This class is not intended for use by modders, Options register themselves.
+ */
+@Deprecated
 public class OptionsRegistry
 {
     public static final Map<String, Map<String, Option<?>>> OPTION_REGISTRY = new HashMap<>();
 
+    /**
+     * @deprecated modders do not use this, options register themselves.
+     * @param modId - the modid
+     * @param option - the option.
+     */
+    @Deprecated
     public static void register(String modId, Option<?> option)
     {
         Map<String, Option<?>> addonRegistry = OPTION_REGISTRY.get(modId);
