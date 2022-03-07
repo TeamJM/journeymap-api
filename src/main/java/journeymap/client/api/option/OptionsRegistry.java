0d25a986aa3d818c93891e00b0bd195b04c7978e
Mysticdrew <drewhaas@gmail.com>
2022-03-06 19:26:10 -0600

    added waypoint getters

--- src/main/java/journeymap/client/api/option/OptionsRegistry.java
+++ src/main/java/journeymap/client/api/option/OptionsRegistry.java
@@ -6,14 +6,14 @@ import java.util.Map;
 /**
  * This class is not intended for use by modders, Options register themselves.
  */
-@Deprecated
 public class OptionsRegistry
 {
     public static final Map<String, Map<String, Option<?>>> OPTION_REGISTRY = new HashMap<>();
 
     /**
-     * @deprecated modders do not use this, options register themselves.
-     * @param modId - the modid
+     * Modders do not use this, options register themselves.
+     *
+     * @param modId  - the modid
      * @param option - the option.
      */
     @Deprecated
