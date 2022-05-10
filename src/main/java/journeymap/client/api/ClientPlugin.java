c65db3a5f7772454933bab677908cfd1ebf87d02
Mysticdrew <drewhaas@gmail.com>
2022-05-10 08:27:34 -0500

    deprecate ClientPlugin annotation

--- src/main/java/journeymap/client/api/ClientPlugin.java
+++ src/main/java/journeymap/client/api/ClientPlugin.java
@@ -8,7 +8,11 @@ import java.lang.annotation.Target;
  * <p>
  * Classes with this annotation must have a no-arg constructor
  * and must also implement the {@link IClientPlugin} interface.
+ *
+ * @deprecated - This annotation is not used in the fabric environment,
+ * it is only included so the common projects build in multi-loader setups.
  */
+@Deprecated
 @Target(ElementType.TYPE)
 public @interface ClientPlugin
 {
