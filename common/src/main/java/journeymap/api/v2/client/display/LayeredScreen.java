6507118a58e694f32f9b063d7d9a65032074ec6b
Mysticdrew <drewhaas@gmail.com>
2024-06-07 09:10:17 -0500

    update changelog and added LayeredScreen

---
+++ common/src/main/java/journeymap/api/v2/client/display/LayeredScreen.java
@@ -0,0 +1,89 @@
+package journeymap.client.ui.component;
+
+import com.mojang.blaze3d.systems.RenderSystem;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.network.chat.Component;
+
+import javax.annotation.Nullable;
+
+public abstract class LayeredScreen extends Screen
+{
+
+    protected Minecraft minecraft;
+    protected Screen backgroundScreen;
+
+    protected LayeredScreen(Component component)
+    {
+        super(component);
+        this.minecraft = Minecraft.getInstance();
+    }
+
+    public void display()
+    {
+        this.backgroundScreen = this.minecraft.screen;
+        this.minecraft.setScreen(this);
+    }
+
+    public void resize(Minecraft minecraft, int width, int height)
+    {
+        super.resize(minecraft, width, height);
+        if (this.backgroundScreen != null)
+        {
+            this.backgroundScreen.resize(this.minecraft, this.width, this.height);
+        }
+    }
+
+    @Override
+    @Deprecated // do not call super.render, call super.renderPopupScreen instead
+    public final void render(GuiGraphics graphics, int mouseX, int mouseY, float partialTicks)
+    {
+        if (this.backgroundScreen != null)
+        // render background screen.
+        {
+            this.backgroundScreen.render(graphics, -1, -1, partialTicks);
+        }
+        // translate z +2000
+        graphics.pose().translate(0.0D, 0.0D, 2000F);
+
+        this.renderPopupScreenBackground(graphics, mouseX, mouseY, partialTicks);
+        this.renderPopupScreen(graphics, mouseX, mouseY, partialTicks);
+    }
+
+    protected void renderPopupScreen(GuiGraphics graphics, int mouseX, int mouseY, float partialTicks)
+    {
+        super.render(graphics, mouseX, mouseY, partialTicks);
+    }
+
+    protected void renderPopupScreenBackground(GuiGraphics graphics, int mouseX, int mouseY, float partialTicks)
+    {
+
+    }
+
+    @Override
+    public final void renderBackground(GuiGraphics graphics, int mouseX, int mouseY, float partialTicks)
+    {
+        // no use
+    }
+
+    @Override
+    public void onClose()
+    {
+        this.popLayer();
+    }
+
+    public void popLayer()
+    {
+        if (this.minecraft.screen != null)
+        {
+            this.minecraft.screen.removed();
+        }
+        this.minecraft.screen = this.backgroundScreen;
+    }
+
+    public Screen getBackgroundScreen()
+    {
+        return this.backgroundScreen;
+    }
+}
