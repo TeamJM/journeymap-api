a3029d6b6e8300ee4d0232d9ed2c0452fe4aa74a
Mysticdrew <drewhaas@gmail.com>
2025-01-19 18:53:31 -0600

    layered screen no longer calling remove on background screen.

--- common/src/main/java/journeymap/api/v2/client/ui/component/LayeredScreen.java
+++ common/src/main/java/journeymap/api/v2/client/ui/component/LayeredScreen.java
@@ -22,7 +22,10 @@ public abstract class LayeredScreen extends Screen
         {
             this.backgroundScreen = this.minecraft.screen;
         }
-        this.minecraft.setScreen(this);
+
+        this.added();
+        this.init(minecraft, minecraft.getWindow().getGuiScaledWidth(), minecraft.getWindow().getGuiScaledHeight());
+        minecraft.getNarrator().sayNow(this.getNarrationMessage());
     }
 
     public void resize(Minecraft minecraft, int width, int height)
