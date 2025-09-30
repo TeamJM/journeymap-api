fd150de333b04a176c70cc0099ffcd77c9582df5
Mysticdrew <drewhaas@gmail.com>
2025-09-30 14:12:30 -0500

    cleanup

--- common/src/main/java/journeymap/api/v2/client/option/BooleanOption.java
+++ common/src/main/java/journeymap/api/v2/client/option/BooleanOption.java
@@ -6,11 +6,11 @@ public class BooleanOption extends Option<Boolean>
 
     /**
      * Boolean - creates a checkbox
-     * For tooltips, your i18n key plus .tooltip in your language json example  mod.option.label and mod.option.label.tooltip
+     * For tooltips, your i18n key plus .tooltip in your language json example: "mod.option.label" and "mod.option.label.tooltip"
      *
      * @param category     - The category;
      * @param fieldName    - The fieldName;
-     * @param label        - The label, can be the display string or i18n key.
+     * @param label        - The label can be the display string or i18n key.
      * @param defaultValue - The default value
      */
     public BooleanOption(OptionCategory category, String fieldName, String label, Boolean defaultValue)
@@ -21,13 +21,13 @@ public class BooleanOption extends Option<Boolean>
 
     /**
      * Boolean - creates a checkbox
-     * For tooltips, your i18n key plus .tooltip in your language json example  mod.option.label and mod.option.label.tooltip
+     * For tooltips, your i18n key plus .tooltip in your language json example: "mod.option.label" and "mod.option.label.tooltip"
      *
      * @param category     - The category;
      * @param fieldName    - The fieldName;
-     * @param label        - The label, can be the display string or i18n key.
+     * @param label        - The label can be the display string or i18n key.
      * @param defaultValue - The default value
-     * @param isMaster     - Makes this value first in the last, and if disabled the whole category is disabled.
+     * @param isMaster     - Makes this value first in the list, and if disabled, the whole category is disabled.
      */
     public BooleanOption(OptionCategory category, String fieldName, String label, Boolean defaultValue, Boolean isMaster)
     {
