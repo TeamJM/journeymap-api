0614d2fa360a04a94d61cd31a7664e0fff2071eb
Mysticdrew <drewhaas@gmail.com>
2026-05-06 16:57:45 -0500

    26.1 add server waypoint-api

---
+++ common/src/test/java/journeymap/api/v2/server/event/server/GlobalWaypointEventTest.java
@@ -0,0 +1,28 @@
+package journeymap.api.v2.server.event.server;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class GlobalWaypointEventTest
+{
+    @Test
+    void createIsCancelable()
+    {
+        var event = new GlobalWaypointEvent(null, GlobalWaypointEvent.Context.CREATE);
+        assertTrue(event.isCancellable());
+    }
+
+    @Test
+    void updateIsCancelable()
+    {
+        var event = new GlobalWaypointEvent(null, GlobalWaypointEvent.Context.UPDATE);
+        assertTrue(event.isCancellable());
+    }
+
+    @Test
+    void deletedIsNotCancelable()
+    {
+        var event = new GlobalWaypointEvent(null, GlobalWaypointEvent.Context.DELETED);
+        assertFalse(event.isCancellable());
+    }
+}
