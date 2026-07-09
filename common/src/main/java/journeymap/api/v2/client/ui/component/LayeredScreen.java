20491400185a69d259c303fb7ebb9a0762306178
Mysticdrew <drewhaas@gmail.com>
2026-07-08 20:46:57 -0500

    Fixed layered screen z translation

--- common/src/main/java/journeymap/api/v2/client/ui/component/LayeredScreen.java
+++ common/src/main/java/journeymap/api/v2/client/ui/component/LayeredScreen.java
@@ -6,6 +6,7 @@ import net.minecraft.client.gui.ScaledResolution;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.util.text.ITextComponent;
 import org.lwjgl.input.Keyboard;
+import org.lwjgl.opengl.GL11;
 
 import java.io.IOException;
 
@@ -67,8 +68,15 @@ public abstract class LayeredScreen extends GuiScreen
         {
             this.backgroundScreen.drawScreen(-1, -1, partialTicks);
         }
-        // translate z +2000
-        GlStateManager.translate(0.0F, 0.0F, 2000F);
+
+        ScaledResolution popupRes = new ScaledResolution(this.minecraft);
+        GlStateManager.clear(GL11.GL_DEPTH_BUFFER_BIT);
+        GlStateManager.matrixMode(GL11.GL_PROJECTION);
+        GlStateManager.loadIdentity();
+        GlStateManager.ortho(0.0D, popupRes.getScaledWidth_double(), popupRes.getScaledHeight_double(), 0.0D, 1000.0D, 21000.0D);
+        GlStateManager.matrixMode(GL11.GL_MODELVIEW);
+        GlStateManager.loadIdentity();
+        GlStateManager.translate(0.0F, 0.0F, -11000.0F);
 
         this.renderPopupScreenBackground(mouseX, mouseY, partialTicks);
         this.renderPopupScreen(mouseX, mouseY, partialTicks);
